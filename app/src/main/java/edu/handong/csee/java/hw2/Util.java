599deff530aeb0a19a445b3c58e37a9afa0e298a
g.maeng <g.maeng@localhost>
2021-05-06 11:17:04 +0900

    push file 1

---
+++ app/src/main/java/edu/handong/csee/java/hw2/Util.java
@@ -0,0 +1,18 @@
+package edu.handong.csee.java.hw2;
+
+/**
+ * return parameter value
+ * @author Myunghun Ahn 
+ */
+
+public class Util {
+
+    /**
+     * string convert to uppercase! 
+     * @param stringValue are KM, M, MILE, TON, KG, G
+     * @return string's uppercase! 
+     */
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+}
